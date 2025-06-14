5f92e6a867278fc4e94e955dd99a777b1ddd0040
Vsevolod Zholobov <vszholobov@gmail.com>
2025-06-14 21:50:31 +0300

    Unroll loop

--- server/asm/simd_multiple_amd64.s
+++ server/asm/simd_multiple_amd64.s
@@ -8,85 +8,28 @@ TEXT ·IntersectsAVXMultiple(SB), NOSPLIT, $0-32
     MOVQ    a+0(FP), R8       // R8 = a
     MOVQ    b+8(FP), R9       // R9 = b
     MOVQ    n+16(FP), CX      // CX = n
-    XORQ    AX, AX            // RAX = count = 0
+    XORQ    AX, AX            // RAX = 0 (64-бит)
+
     XORQ    R10, R10          // R10 = i = 0
 
-    MOVQ    CX, R11
-    SHRQ    $2, R11           // R11 = n/4 (кол-во полных блоков по 4)
-    ANDQ    $3, CX            // CX = остаток n % 4
+loop:
+    CMPQ    R10, CX
+    JAE     done
 
-    TESTQ   R11, R11
-    JE      loop_tail         // Если нет блоков по 4, сразу к хвосту
+    MOVQ    (R8)(R10*8), R11
+    MOVQ    (R9)(R10*8), R12
 
-loop_main:
-    // Первый элемент
-    MOVQ    (R8)(R10*8), R12
-    MOVQ    (R9)(R10*8), R13
-    VMOVDQU (R12), Y0
-    VMOVDQU (R13), Y1
+    VMOVDQU (R11), Y0
+    VMOVDQU (R12), Y1
     VPAND   Y1, Y0, Y0
     VPTEST  Y0, Y0
-    JZ      skip0
+    JZ      skip
+
     INCQ    AX
-skip0:
+
+skip:
     INCQ    R10
-
-    // Второй элемент
-    MOVQ    (R8)(R10*8), R12
-    MOVQ    (R9)(R10*8), R13
-    VMOVDQU (R12), Y0
-    VMOVDQU (R13), Y1
-    VPAND   Y1, Y0, Y0
-    VPTEST  Y0, Y0
-    JZ      skip1
-    INCQ    AX
-skip1:
-    INCQ    R10
-
-    // Третий элемент
-    MOVQ    (R8)(R10*8), R12
-    MOVQ    (R9)(R10*8), R13
-    VMOVDQU (R12), Y0
-    VMOVDQU (R13), Y1
-    VPAND   Y1, Y0, Y0
-    VPTEST  Y0, Y0
-    JZ      skip2
-    INCQ    AX
-skip2:
-    INCQ    R10
-
-    // Четвертый элемент
-    MOVQ    (R8)(R10*8), R12
-    MOVQ    (R9)(R10*8), R13
-    VMOVDQU (R12), Y0
-    VMOVDQU (R13), Y1
-    VPAND   Y1, Y0, Y0
-    VPTEST  Y0, Y0
-    JZ      skip3
-    INCQ    AX
-skip3:
-    INCQ    R10
-
-    DECQ    R11
-    JNZ     loop_main
-
-loop_tail:
-    TESTQ   CX, CX
-    JE      done
-
-loop_tail_loop:
-    MOVQ    (R8)(R10*8), R12
-    MOVQ    (R9)(R10*8), R13
-    VMOVDQU (R12), Y0
-    VMOVDQU (R13), Y1
-    VPAND   Y1, Y0, Y0
-    VPTEST  Y0, Y0
-    JZ      skip_tail
-    INCQ    AX
-skip_tail:
-    INCQ    R10
-    DECQ    CX
-    JNZ     loop_tail_loop
+    JMP     loop
 
 done:
     VZEROUPPER
