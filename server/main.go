07d2b007f9b4fa52365a03a25607c3f59022f256
Vsevolod Zholobov <vszholobov@gmail.com>
2025-06-15 01:13:10 +0300

    fix asm

--- server/main.go
+++ server/main.go
@@ -5,9 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"tetrisServer/avx/asm"
 	"tetrisServer/server"
-	"unsafe"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
@@ -17,64 +15,6 @@ import (
 )
 
 func main() {
-	// Пример с данными из [][]uint16
-	e := make([]uint16, 16)
-	f := make([]uint16, 16)
-	e[3] = 0xFFFF
-	f[3] = 0x0001
-
-	// Преобразуем в *[16]uint16
-	ptr1 := (*[16]uint16)(unsafe.Pointer(&e[0]))
-	ptr2 := (*[16]uint16)(unsafe.Pointer(&f[0]))
-
-	result := asm.IntersectsAVXSingle(ptr1, ptr2)
-	fmt.Println("intersects (should be true):", result)
-
-	g := [16]uint16{}
-	h := [16]uint16{}
-
-	g[5] = 0x1234
-	h[5] = 0x1234
-
-	result = asm.IntersectsAVXSingle(&g, &h)
-	fmt.Println("intersects (should be true):", result)
-
-	h[5] = 0x0
-	result = asm.IntersectsAVXSingle(&g, &h)
-	fmt.Println("intersects (should be false):", result)
-
-	N := 100
-	c := make([]*[16]uint16, N)
-	d := make([]*[16]uint16, N)
-
-	for i := 0; i < N; i++ {
-		arr1 := [16]uint16{}
-		arr2 := [16]uint16{}
-
-		// Пример заполнения
-		if i%2 == 0 {
-			arr1[3] = 0xFF
-			arr2[3] = 0xFF
-		}
-
-		c[i] = &arr1
-		d[i] = &arr2
-	}
-
-	count := asm.IntersectsAVXMultiple(&c[0], &d[0], N)
-	fmt.Println("Intersections:", count)
-
-	a := make([]uint16, 16)
-	b := make([]uint16, 16)
-	a[5] = 0x0004
-	b[5] = 0x0004
-
-	if intersectsSimdeSingle(a, b) {
-		fmt.Println("Пересечение есть")
-	} else {
-		fmt.Println("Пересечения нет")
-	}
-
 	logFile := openLogFile()
 	defer logFile.Close()
 	log.SetOutput(logFile)
