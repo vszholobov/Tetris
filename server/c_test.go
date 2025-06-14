b3d194264853474f67d33deac50033da4ae25d45
Vsevolod Zholobov <vszholobov@gmail.com>
2025-06-14 17:43:16 +0300

    c_test.go Noinline

--- server/c_test.go
+++ server/c_test.go
@@ -10,32 +10,70 @@ import (
 	"tetrisServer/asm"
 )
 
-// ваша C‑SIMD-функция
-// //go:noescape
-// func intersectsSimde(a, b []uint16) bool
-
-// новая обёртка для asm
-func intersectsAsm(a, b []uint16) bool {
-	if len(a) < 16 || len(b) < 16 {
-		panic("need at least 16 elements")
-	}
-	// приводим к массивам [16]uint16
-	return asm.IntersectsAVX((*[16]uint16)(unsafe.Pointer(&a[0])), (*[16]uint16)(unsafe.Pointer(&b[0])))
-}
-
-func intersectsBigInt(a, b *big.Int) bool {
-	var tmp big.Int
-	tmp.And(a, b)
-	return tmp.Sign() != 0
-}
-
-func intersectsGo(a, b []uint16) bool {
-	for i := 0; i < 16; i++ {
-		if a[i]&b[i] != 0 {
-			return true
+//go:noinline
+func processBigInt(data []*big.Int) int {
+	sum := 0
+	for j := 0; j < len(data)-1; j++ {
+		var tmp big.Int
+		tmp.And(data[j], data[j+1])
+		if tmp.Sign() != 0 {
+			sum++
 		}
 	}
-	return false
+	return sum
+}
+
+//go:noinline
+func processGo(data [][]uint16) int {
+	sum := 0
+	for j := 0; j < len(data)-1; j++ {
+		a, b := data[j], data[j+1]
+		for i := 0; i < 16; i++ {
+			if a[i]&b[i] != 0 {
+				sum++
+				break
+			}
+		}
+	}
+	return sum
+}
+
+//go:noinline
+func processSimde(data [][]uint16) int {
+	sum := 0
+	for j := 0; j < len(data)-1; j++ {
+		if intersectsSimde(data[j], data[j+1]) {
+			sum++
+		}
+	}
+	return sum
+}
+
+//go:noinline
+func processAsm(data [][]uint16) int {
+	sum := 0
+	for j := 0; j < len(data)-1; j++ {
+		// приводим к *[16]uint16
+		a := (*[16]uint16)(unsafe.Pointer(&data[j][0]))
+		b := (*[16]uint16)(unsafe.Pointer(&data[j+1][0]))
+		if asm.IntersectsAVX(a, b) {
+			sum++
+		}
+	}
+	return sum
+}
+
+func generateData(n int) [][]uint16 {
+	rand.Seed(time.Now().UnixNano())
+	data := make([][]uint16, n)
+	for i := range data {
+		arr := make([]uint16, 16)
+		for j := range arr {
+			arr[j] = uint16(rand.Intn(0xFFFF))
+		}
+		data[i] = arr
+	}
+	return data
 }
 
 func prepareBigIntFromSlice(data []uint16) *big.Int {
@@ -48,34 +86,15 @@ func prepareBigIntFromSlice(data []uint16) *big.Int {
 	return b
 }
 
-func generateData(n int) [][]uint16 {
-	rand.Seed(time.Now().UnixNano())
-	data := make([][]uint16, n)
-	for i := 0; i < n; i++ {
-		arr := make([]uint16, 16)
-		for j := 0; j < 16; j++ {
-			arr[j] = uint16(rand.Intn(0xFFFF))
-		}
-		data[i] = arr
-	}
-	return data
-}
-
 func BenchmarkIntersectsBigIntLarge(b *testing.B) {
 	data := generateData(1_000_000)
 	bigData := make([]*big.Int, len(data))
 	for i, arr := range data {
 		bigData[i] = prepareBigIntFromSlice(arr)
 	}
-
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		sum := 0
-		for j := 0; j < len(bigData)-1; j++ {
-			if intersectsBigInt(bigData[j], bigData[j+1]) {
-				sum++
-			}
-		}
+		sum := processBigInt(bigData)
 		if sum == 0 {
 			b.Fatalf("impossible")
 		}
@@ -84,15 +103,9 @@ func BenchmarkIntersectsBigIntLarge(b *testing.B) {
 
 func BenchmarkIntersectsGoLarge(b *testing.B) {
 	data := generateData(1_000_000)
-
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		sum := 0
-		for j := 0; j < len(data)-1; j++ {
-			if intersectsGo(data[j], data[j+1]) {
-				sum++
-			}
-		}
+		sum := processGo(data)
 		if sum == 0 {
 			b.Fatalf("impossible")
 		}
@@ -101,15 +114,9 @@ func BenchmarkIntersectsGoLarge(b *testing.B) {
 
 func BenchmarkIntersectsSimdeLarge(b *testing.B) {
 	data := generateData(1_000_000)
-
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		sum := 0
-		for j := 0; j < len(data)-1; j++ {
-			if intersectsSimde(data[j], data[j+1]) {
-				sum++
-			}
-		}
+		sum := processSimde(data)
 		if sum == 0 {
 			b.Fatalf("impossible")
 		}
@@ -118,15 +125,9 @@ func BenchmarkIntersectsSimdeLarge(b *testing.B) {
 
 func BenchmarkIntersectsAsmLarge(b *testing.B) {
 	data := generateData(1_000_000)
-
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		sum := 0
-		for j := 0; j < len(data)-1; j++ {
-			if intersectsAsm(data[j], data[j+1]) {
-				sum++
-			}
-		}
+		sum := processAsm(data)
 		if sum == 0 {
 			b.Fatalf("impossible")
 		}
