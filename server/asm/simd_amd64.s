5218279a1f1a30afee634ed6ad77e109a317ec3b
Vsevolod Zholobov <vszholobov@gmail.com>
2025-06-14 17:36:11 +0300

    Fix asm

--- server/asm/simd_amd64.s
+++ server/asm/simd_amd64.s
@@ -4,15 +4,24 @@
 #include "textflag.h"
 
 // func IntersectsAVX(a, b *[16]uint16) bool
-TEXT ·IntersectsAVX(SB), NOSPLIT, $0-24
-    VMOVDQU (DI), Y0
-    VMOVDQU (SI), Y1
-    VPAND  Y1, Y0, Y0
-    VPTEST Y0, Y0
-    JNZ    nonzero
+TEXT ·IntersectsAVX(SB), NOSPLIT, $0-40
+    // Загрузим из стека аргументы:
+    MOVQ    a+0(FP), RDI   // первый аргумент -> RDI
+    MOVQ    b+8(FP), RSI   // второй аргумент -> RSI
+
+    // Теперь RDI и RSI указывают на [16]uint16
+    VMOVDQU (RDI), Y0      // load 256 бит from a
+    VMOVDQU (RSI), Y1      // load 256 бит from b
+
+    VPAND   Y1, Y0, Y0     // v0 = v0 & v1
+    VPTEST  Y0, Y0         // test v0 == 0 ?
+
+    JNZ     nonzero
+
 zero:
-    MOVB   $0, AL
+    MOVB    $0, AL         // result = false
     RET
+
 nonzero:
-    MOVB   $1, AL
+    MOVB    $1, AL         // result = true
     RET
