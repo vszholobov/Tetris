c5346887127bdb560622da639d63b26c083197e7
Vsevolod Zholobov <vszholobov@gmail.com>
2025-06-14 20:36:39 +0300

    Optimize single_asm

--- server/asm/simd_single_amd64.s
+++ server/asm/simd_single_amd64.s
@@ -5,26 +5,19 @@
 
 // func IntersectsAVX(a, b *[16]uint16) bool
 TEXT ·IntersectsAVXSingle(SB), NOSPLIT, $0-32
-    // Считаем a → DI, b → SI
     MOVQ    a+0(FP), DI
     MOVQ    b+8(FP), SI
-
-    // Загружаем 256 бит
     VMOVDQU (DI), Y0
     VMOVDQU (SI), Y1
 
-    // Побитовое AND: Y0 = Y0 & Y1
     VPAND   Y1, Y0, Y0
-
-    // Проверка на ноль
     VPTEST  Y0, Y0
+    SETNE   AL
+    MOVB    AL, ret+16(FP)
 
-    // AL := (Y0 != 0)
-    SETNE   R8B
-    // Записываем результат в ret
-    MOVB    R8B, ret+16(FP)
+//    SETNE   R8B
+//    MOVB    R8B, ret+16(FP)
 
     // Очистка AVX
     VZEROUPPER
-
     RET
