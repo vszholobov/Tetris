e53f0ca23cf034696f984a0e6aa4430fdc7f2ea8
Vsevolod Zholobov <vszholobov@gmail.com>
2025-11-21 17:40:43 +0300

    Test field.Bytes on random fields

--- server/field/field_test.go
+++ server/field/field_test.go
@@ -1,7 +1,9 @@
 package field
 
 import (
+	"crypto/rand"
 	"flag"
+	"math/big"
 	"os"
 	"strings"
 	"testing"
@@ -108,6 +110,65 @@ func TestTopBorderClosed(t *testing.T) {
 		rotatedRight := field.RotatePiece(PieceRotateRight)
 		if rotatedRight {
 			t.Errorf("Can rotate piece %d right", pieceType)
+		}
+	}
+}
+
+func bigIntToBytes32(bi *big.Int) [32]byte {
+	var arr [32]byte
+	b := bi.Bytes()
+	copy(arr[32-len(b):], b)
+	return arr
+}
+
+func randomBigInt252() *big.Int {
+	randBits := new(big.Int)
+	max := new(big.Int).Lsh(big.NewInt(1), 251)
+	max.Sub(max, big.NewInt(1))
+	n, err := rand.Int(rand.Reader, max)
+	if err != nil {
+		panic(err)
+	}
+	randBits.Set(n)
+
+	firstBit := new(big.Int).Lsh(big.NewInt(1), 251)
+	randBits.Or(randBits, firstBit)
+	return randBits
+}
+
+func bigIntToBinStr252(bi *big.Int) string {
+	s := bi.Text(2)
+	if len(s) < 256 {
+		s = strings.Repeat("0", 256-len(s)) + s
+	}
+	return s
+}
+
+func TestMakeFieldRandom252Bits(t *testing.T) {
+	total := *N
+	step := total / 10
+	if step == 0 {
+		step = 1
+	}
+
+	for i := 0; i < total; i++ {
+		if (i+1)%(step) == 0 {
+			percent := (i + 1) * 100 / total
+			t.Logf("TestMakeFieldRandom252Bits progress %d", percent)
+		}
+
+		n := randomBigInt252()
+		binStr := bigIntToBinStr252(n)
+		field := MakeField(fixedPieceSelector{pieceType: lib.IShape}, binStr)
+
+		pieceVal := big.NewInt(240)
+		b := field.Bytes()
+		fieldAndPiece := big.NewInt(0).Or(big.NewInt(0).SetBytes(b[:]), pieceVal)
+		expected := bigIntToBytes32(fieldAndPiece)
+		actual := field.Bytes()
+
+		if actual != expected {
+			t.Fatalf("iter %d: Bytes() mismatch\nexpected: %v\n actual: %v\nbinStr: %s", i, expected, actual, binStr)
 		}
 	}
 }
